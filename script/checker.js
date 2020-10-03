6848b8e940f2d30ec8a911441a0ead996218c4b9
UserUnknown <50655871+UserUnknownX@users.noreply.github.com>
2020-10-03 15:38:01 +0800

    Create checker.js

---
+++ script/checker.js
@@ -0,0 +1,31 @@
+function set_Cookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+} 
+
+function get_Cookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+         }
+         if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+         }
+     }
+    return "";
+} 
+
+function change_it(on_or_off){
+    if(on_or_off==1){
+        console.log("Trun OFf");
+    }
+    if(on_or_off==0){
+        console.log("Trun On");
+    } 
+}
